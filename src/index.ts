bc803b2d7c25b680083166f3e67bf9c14b901f3c
Rick Ansay <rick.ansay@gmail.com>
2022-06-16 22:50:41 -0600

    Enum match result options

--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,23 @@ const matches = fs
     (row: string): string[] => {
     return row.split(',');
   }
-  );
+);
+
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+};
 
 let manUnitedWins = 0;
 
 for (let match of matches) {
-  if (match[1] === 'Man United' && match[5] === 'H') {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === 'A') {
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
-}
+};
 
 console.log(`Man United won ${manUnitedWins} games`);
 
