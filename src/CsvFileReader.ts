36727807b281b2018e5c6548e1c4555a3be44566
Rick Ansay <rick.ansay@gmail.com>
2022-06-19 11:21:42 -0600

    Move map method to MatchReader

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,11 +1,7 @@
 import fs from 'fs';
 
-
-
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-
 export class CsvFileReader {
-  data: string[] = [];
+  data: string[][] = [];
   
   constructor(public filename: string) {}
 
@@ -20,16 +16,5 @@ export class CsvFileReader {
         return row.split(',');
       }
     )
-    .map((row: string[]): MatchData => {
-      return [
-        dateStringToDate(row[0]),
-        row[1],
-        row[2], 
-        parseInt(row[3]),
-        parseInt(row[4]),
-        row[5] as MatchResult,
-        row[6]
-      ];
-    });
   }
 }
